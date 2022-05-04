32f15b3b699c0c5f3d23876b36335664b328a256
DeveloperMatheus97 <matheuscaitanosouza18@outlook.com>
2022-05-04 13:42:32 -0300

    first commit

---
+++ Ex04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, horas, resto, minutos, segundos;
+
+            Console.WriteLine("DIGITE OS SEGUNDOS");
+            n = int.Parse(Console.ReadLine());
+
+            horas = n / 3600;
+            resto = n % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+
+            Console.ReadLine();
+        }
+    }
+}
